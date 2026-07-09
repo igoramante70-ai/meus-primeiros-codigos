da2c7a6538230a8eafa0a415630dd6e84494db38
igoramante70-ai <igoramante70@gmail.com>
2026-07-09 19:36:43 +0200

    Adiciona calculadora com 4 operações

---
+++ calculadora.py
@@ -0,0 +1,15 @@
+print("=== MINHA CALCULADORA ===")
+
+num1 = float(input("Digite o primeiro número:"))
+num2 = float(input("Digite o segundo número:"))
+
+soma = num1 + num2
+subtracao = num1 - num2
+multiplicacao = num1 * num2
+divisao = num1 / num2
+
+print(f"\nResultados:")
+print(f"soma: {soma}")
+print(f"Subtracao: {subtracao}")
+print(f"Multiplicacao: {multiplicacao}")
+print(f"Divisao: {divisao}")
